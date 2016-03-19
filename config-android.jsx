da396bef33cac55d659afa690f1553fff52880e0
Erin Sparling <erin@everyplace.net>
2016-03-18 22:32:22 -0400

    Added configs

---
+++ config-android.jsx
@@ -0,0 +1,11 @@
+var android = function() {
+  var config = {
+    "sizes":[
+      {"x":468,"y":255,"name":"branding_logo_large.png","subFolder":"Large"},
+      {"x":624,"y":340,"name":"branding_logo_large.png","subFolder":"xLarge"}
+    ],
+    "confirm":false,
+    "outputFolder":"android"
+  }
+  return config;
+}
