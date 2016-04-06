301c7517c09c78f99dc6a0e7c8edb2d1707fb5a9
Erin Sparling <erin@everyplace.net>
2016-04-06 12:12:47 -0400

    Adding further config and examples

---
+++ config-launchimage-ios.jsx
@@ -0,0 +1,12 @@
+var iOSLaunchImage = function() {
+  var config = {
+    "sizes":[
+      {"x":600,"y":600,"name":"LaunchImage.png"},
+      {"x":1200,"y":1200,"name":"LaunchImage@2x.png"},
+      {"x":1800,"y":1800,"name":"LaunchImage@3x.png"}
+    ],
+    "confirm":false,
+    "outputFolder":"iOS"
+  }
+  return config;
+}
