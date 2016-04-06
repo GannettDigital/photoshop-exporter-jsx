4d7ace78f9dbd78d1725555135f00681c6c1cac4
Erin Sparling <erin@everyplace.net>
2016-04-06 12:16:55 -0400

    All iOS configs

---
+++ config-logowhite-ios.jsx
@@ -0,0 +1,12 @@
+var iOSLogoWhite = function() {
+  var config = {
+    "sizes":[
+      {"x":172,"y":32,"name":"logo_white.png"},
+      {"x":340,"y":64,"name":"logo_white@2x.png"},
+      {"x":510,"y":96,"name":"logo_white@3x.png"}
+    ],
+    "confirm":false,
+    "outputFolder":"iOS"
+  }
+  return config;
+}
