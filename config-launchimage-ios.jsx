2b37316dd7a8d4c5fd7198a3dd7298429d1e992b
Erin Sparling <erin@everyplace.net>
2016-04-12 15:54:25 -0400

    Updatd sizes for LaunchPhone and LaunchPad for iOS

--- config-launchimage-ios.jsx
+++ config-launchimage-ios.jsx
@@ -1,9 +1,11 @@
 var iOSLaunchImage = function() {
   var config = {
     "sizes":[
-      {"x":600,"y":600,"name":"LaunchImage.png"},
-      {"x":1200,"y":1200,"name":"LaunchImage@2x.png"},
-      {"x":1800,"y":1800,"name":"LaunchImage@3x.png"}
+      {"x":260,"y":260,"name":"LaunchPhone.png"},
+      {"x":520,"y":520,"name":"LaunchPhone@2x.png"},
+      {"x":1000,"y":1000,"name":"LaunchPhone@3x.png"},
+      {"x":600,"y":600,"name":"LaunchPad.png"},
+      {"x":1200,"y":1200,"name":"LaunchPad@2x.png"}
     ],
     "confirm":false,
     "outputFolder":"iOS",
