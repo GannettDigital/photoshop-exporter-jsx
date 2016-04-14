1c17226de22cddfa2716c417662794a111d9c1b1
Sparling <esparling@gci-thdloan05-m.usatoday.us.ad.gannett.com>
2016-04-14 16:14:17 -0400

    Recovering from lost work

--- config-logowhite-android.jsx
+++ config-logowhite-android.jsx
@@ -1,10 +1,18 @@
 var androidLogoWhite = function() {
   var config = {
     "sizes":[
-      {"x":255,"y":48,"name":"ab_logo_default.png","subFolder":"drawable-hdpi"},
-      {"x":340,"y":64,"name":"ab_logo_default.png","subFolder":"drawable-xhdpi"},
-      {"x":510,"y":96,"name":"ab_logo_default.png","subFolder":"drawable-xxhdpi"},
-      {"x":680,"y":128,"name":"ab_logo_default.png","subFolder":"drawable-xxxhdpi"}
+      {"x":255,"y":48,"name":"logo_default.png","subFolder":"drawable-hdpi"},
+      {"x":340,"y":64,"name":"logo_default.png","subFolder":"drawable-xhdpi"},
+      {"x":510,"y":96,"name":"logo_default.png","subFolder":"drawable-xxhdpi"},
+      {"x":680,"y":128,"name":"logo_default.png","subFolder":"drawable-xxxhdpi"},
+      {"x":255,"y":48,"name":"logo_default_trans.png","subFolder":"drawable-hdpi"},
+      {"x":340,"y":64,"name":"logo_default_trans.png","subFolder":"drawable-xhdpi"},
+      {"x":510,"y":96,"name":"logo_default_trans.png","subFolder":"drawable-xxhdpi"},
+      {"x":680,"y":128,"name":"logo_default_trans.png","subFolder":"drawable-xxxhdpi"},
+      {"x":112,"y":21,"name":"widget_header.png","subFolder":"drawable-hdpi"},
+      {"x":149,"y":28,"name":"widget_header.png","subFolder":"drawable-xhdpi"},
+      {"x":224,"y":42,"name":"widget_header.png","subFolder":"drawable-xxhdpi"},
+      {"x":298,"y":56,"name":"widget_header.png","subFolder":"drawable-xxxhdpi"}
     ],
     "confirm":false,
     "outputFolder":"Android",
